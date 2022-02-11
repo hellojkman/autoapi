37a58952c6cdb0f871d7c73783343fa6ae2c9348
hellojkman <sun-up12@hanmail.net>
2022-02-11 00:17:09 +0000

    auto node play and parking api

--- node.sh
+++ node.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-node app.js &
+node=$(ps -ef | grep 'node app.js')
+
+first1=$(echo ${node} | cut -d " " -f2)
+first2=$(echo ${node} | cut -d " " -f8)
+echo $first2
+if [ $first2 == "node" ]
+then
+	for var in $first1
+	do
+		echo $var
+ 		if [ -n ${var} ]
+		then
+ 			result=$(kill -9 ${var})
+			echo "${var} process is killed."
+			node app.js &
+		fi
+ 	done
+ else
+ 	node app.js &
+ fi
